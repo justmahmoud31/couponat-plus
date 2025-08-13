3de0bec1f7baf8cbcd9b2af4aeff9ba8d7f4c081
Just Mahmoud <142663291+justmahmoud31@users.noreply.github.com>
2025-08-14 01:38:15 +0300

    Update dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -2,9 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    // await mongoose.connect(
-    //   "mongodb+srv://justMahmoud:Mahmoud3152003@cluster0.hua85.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    // );
     await mongoose.connect("mongodb://0.0.0.0:27017/coupons");
     console.log("✅ Database Connected");
   } catch (error) {
